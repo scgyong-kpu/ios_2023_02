a804409dcb73ad2394f11d280b99419e094a6831
Kiyong Kim <scgyong.isu@gmail.com>
2023-01-30 01:23:57 +0900

    B-2. if-let

--- SwiftTour/Tour_03.playground/Contents.swift
+++ SwiftTour/Tour_03.playground/Contents.swift
@@ -4,9 +4,8 @@ let ages = [ "Kelly": 13, "Mac": 18, "John": 34, "Kim": 29 ]
 var cost = 0
 let names = [ "Kelly", "Grace", "Kim" ]
 for name in names {
-    let age = ages[name]
-    if age != nil {
-        if age! > 19 {
+    if let age = ages[name] {
+        if age > 19 {
             cost += 20
         } else {
             cost += 15

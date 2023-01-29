712989e83cc7ed577305b24a0b77808a44ee68c2
Kiyong Kim <scgyong.isu@gmail.com>
2023-01-30 00:09:11 +0900

    E. 명시적 배열 타입

--- SwiftTour/Tour_02.playground/Contents.swift
+++ SwiftTour/Tour_02.playground/Contents.swift
@@ -29,3 +29,20 @@ a
 b
 c
 
+let integers = [ 1, 2 ]
+type(of: integers)
+
+let doubles = [ 1.0, 2 ]
+type(of: doubles)
+
+let x = 1, y = 2
+let xy = [ x, y ]
+type(of: xy)
+
+let xy2: [Double] = [ 1, 2 ]
+type(of: xy2)
+
+let xy3: [Double] = [ x, y ]
+type(of: xy3)
+
+

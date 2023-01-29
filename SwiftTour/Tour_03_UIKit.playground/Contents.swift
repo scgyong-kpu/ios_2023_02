39b54ffe72b8bfe4a28a3fef4ec2226f679b8a43
Kiyong Kim <scgyong.isu@gmail.com>
2023-01-30 01:32:00 +0900

    B-4. Early-return

--- SwiftTour/Tour_03_UIKit.playground/Contents.swift
+++ SwiftTour/Tour_03_UIKit.playground/Contents.swift
@@ -5,16 +5,15 @@ let indexPath = IndexPath(row: 0, section: 0)
 let urlString = "https://some_host.com/file.png"
 let queue = OperationQueue()
 queue.addOperation {
-    if let imageUrl = URL(string: urlString) {
-        if let imageData = try? Data(contentsOf: imageUrl) {
-            if let image = UIImage(data: imageData) {
-                let mainQueue = OperationQueue.main
-                mainQueue.addOperation {
-                    if let cell = tableView.cellForRow(at: indexPath) {
-                        cell.imageView?.image = image
-                    }
-                }
-            }
-        }
+    let imageUrl = URL(string: urlString)
+    if imageUrl == nil { return }
+    let imageData = try? Data(contentsOf: imageUrl!)
+    if imageData == nil { return }
+    let image = UIImage(data: imageData!)
+    if image == nil { return }
+    let mainQueue = OperationQueue.main
+    mainQueue.addOperation {
+        let cell = tableView.cellForRow(at: indexPath)
+        cell?.imageView?.image = image
     }
 }

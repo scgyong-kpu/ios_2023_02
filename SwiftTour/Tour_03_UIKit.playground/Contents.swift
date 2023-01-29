dfeb59ae2d5bdc4c284ca86ce465d045fa741341
Kiyong Kim <scgyong.isu@gmail.com>
2023-01-30 01:34:44 +0900

    B-5. Early-return: guard let

--- SwiftTour/Tour_03_UIKit.playground/Contents.swift
+++ SwiftTour/Tour_03_UIKit.playground/Contents.swift
@@ -5,15 +5,12 @@ let indexPath = IndexPath(row: 0, section: 0)
 let urlString = "https://some_host.com/file.png"
 let queue = OperationQueue()
 queue.addOperation {
-    let imageUrl = URL(string: urlString)
-    if imageUrl == nil { return }
-    let imageData = try? Data(contentsOf: imageUrl!)
-    if imageData == nil { return }
-    let image = UIImage(data: imageData!)
-    if image == nil { return }
+    guard let imageUrl = URL(string: urlString) else { return }
+    guard let imageData = try? Data(contentsOf: imageUrl) else { return }
+    guard let image = UIImage(data: imageData) else { return }
     let mainQueue = OperationQueue.main
     mainQueue.addOperation {
-        let cell = tableView.cellForRow(at: indexPath)
-        cell?.imageView?.image = image
+        guard let cell = tableView.cellForRow(at: indexPath) else { return }
+        cell.imageView?.image = image
     }
 }

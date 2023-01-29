6b82fb903acf1cebb7279cdb7446e0205f5610a1
Kiyong Kim <scgyong.isu@gmail.com>
2023-01-29 23:25:51 +0900

    C. array 를 var 로 선언하여 값 변경 가능하게 하기

--- SwiftTour/Tour_02.playground/Contents.swift
+++ SwiftTour/Tour_02.playground/Contents.swift
@@ -1,6 +1,6 @@
 import Foundation
 
-let names: [String] = [ "Kim", "Lee", "Park" ]
+var names: [String] = [ "Kim", "Lee", "Park" ]
 names.append("Choi")
 
 let kim = names[0]
@@ -11,4 +11,12 @@ names.first
 names.last
 
 names.sorted()
+
+var nums = [ 1, 2, 3, 4, 5 ]
+nums.append(67)
+nums[3] = 100
+
+nums
+nums.sort()
+nums
 

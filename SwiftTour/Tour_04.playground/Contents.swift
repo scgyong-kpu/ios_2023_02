ed4f5bffea5ee5ce4824d338bcf68f62e12828df
Kiyong Kim <scgyong.isu@gmail.com>
2023-01-30 21:30:07 +0900

    B. Variadic argument

--- SwiftTour/Tour_04.playground/Contents.swift
+++ SwiftTour/Tour_04.playground/Contents.swift
@@ -7,3 +7,14 @@ func greet(name: String, day: String) -> String {
 let gr1 = greet(name: "Dave", day: "Monday")
 let gr2 = greet(name: "Kelly", day: "Sunday")
 let gr3 = greet
+
+func average(numbers: Int...) -> Double {
+    var sum = 0
+    for num in numbers {
+        sum += num
+    }
+    return Double(sum) / Double(numbers.count)
+}
+let avg1 = average(numbers: 10, 20, 23)
+let avg2 = average(numbers: 1, 2, 3, 4, 5, 6, 10)
+

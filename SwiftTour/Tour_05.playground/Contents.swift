634c2726553445f6271c03b839004d3e22ae028b
Kiyong Kim <scgyong.isu@gmail.com>
2023-01-30 23:55:52 +0900

    A. Introducing class Animal

--- SwiftTour/Tour_05.playground/Contents.swift
+++ SwiftTour/Tour_05.playground/Contents.swift
@@ -1,2 +1,12 @@
 import Foundation
 
+class Animal {
+    var legCount = 0
+    func simpleDesc() -> String {
+        "This animal has \(legCount) legs"
+    }
+}
+
+let monster = Animal()
+monster.legCount = 10
+monster.simpleDesc()

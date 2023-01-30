639ca8df77ada4e2331d5ceb49368ba4d51d7e87
Kiyong Kim <scgyong.isu@gmail.com>
2023-01-30 22:40:34 +0900

    H. Introducing closure

---
+++ SwiftTour/Tour_04_Closure.playground/Contents.swift
@@ -0,0 +1,28 @@
+import Foundation
+
+func points(number: Int, bonus: (Int)->Bool) -> Int {
+    var pt = number * 10
+    if bonus(number) {
+        pt += 1
+    }
+    return pt
+}
+
+let pt1 = points(number: 5, bonus: {
+    (number: Int) -> Bool in
+    return number % 2 != 0
+})
+
+let negative = { (num: Int) -> Bool in
+    return num < 0
+}
+let pt2 = points(number: -1, bonus: negative)
+
+let positive = { $0 > 0 }
+let pt3 = points(number: 9, bonus: positive)
+
+let mod3 = points(number: 9, bonus: { $0 % 3 == 0 })
+mod3
+let mod7 = points(number: 9) { $0 % 7 == 0 }
+mod7
+

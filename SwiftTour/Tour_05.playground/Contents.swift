01b195760c16f6e0eaf154a5ee6e6eabca319a30
Kiyong Kim <scgyong.isu@gmail.com>
2023-01-31 00:41:36 +0900

    C. Inheritance

--- SwiftTour/Tour_05.playground/Contents.swift
+++ SwiftTour/Tour_05.playground/Contents.swift
@@ -14,3 +14,28 @@ class Animal {
 let lark = Animal(name: "Lark")
 lark.legCount = 2
 lark.simpleDesc()
+
+// C. Inheritance
+
+class Mammal: Animal {
+    override init(name: String) {
+        super.init(name: name)
+        legCount = 4
+    }
+    override func simpleDesc() -> String {
+        "A mammal named \(name)"
+    }
+    func giveBirth() -> Mammal {
+        print("Brought forth a baby")
+        return baby()
+    }
+    func baby() -> Mammal {
+        Mammal(name: "A baby \(name)")
+    }
+}
+
+let el = Mammal(name: "Elephant")
+el.legCount
+el.simpleDesc()
+let baby_el = el.giveBirth()
+baby_el.simpleDesc()

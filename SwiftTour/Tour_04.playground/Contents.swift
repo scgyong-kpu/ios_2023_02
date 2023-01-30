619ec82c5ea5f489f41f203e68967c52142c21cc
Kiyong Kim <scgyong.isu@gmail.com>
2023-01-30 21:46:44 +0900

    D: a function returning a tuple

--- SwiftTour/Tour_04.playground/Contents.swift
+++ SwiftTour/Tour_04.playground/Contents.swift
@@ -34,3 +34,17 @@ point.1
 point.x
 point.y
 point
+
+// D: a function returning a tuple
+
+func sum_average(numbers: Int...) -> (sum: Int, avg: Double) {
+    var sum = 0
+    for num in numbers {
+        sum += num
+    }
+    let avg = Double(sum) / Double(numbers.count)
+    return (sum: sum, avg: avg)
+}
+let sa1 = sum_average(numbers: 10, 20, 23)
+let sa2 = sum_average(numbers: 1, 2, 3, 4, 5, 6, 10)
+sa1

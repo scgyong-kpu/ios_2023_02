d6d5f14f7871a2f6d2c922175656b13f854fa073
Kiyong Kim <scgyong.isu@gmail.com>
2023-01-30 21:19:30 +0900

    A. function definition: greet(name:day:)

---
+++ SwiftTour/Tour_04.playground/Contents.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+func greet(name: String, day: String) -> String {
+    return "Hello \(name), today is \(day)"
+}
+
+let gr1 = greet(name: "Dave", day: "Monday")
+let gr2 = greet(name: "Kelly", day: "Sunday")
+let gr3 = greet

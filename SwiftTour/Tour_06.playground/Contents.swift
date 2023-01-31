163ab5eaa69629b80e9506eeb04dc19ec8d070a7
Kiyong Kim <scgyong.isu@gmail.com>
2023-01-31 21:51:16 +0900

    A-3. func

--- SwiftTour/Tour_06.playground/Contents.swift
+++ SwiftTour/Tour_06.playground/Contents.swift
@@ -6,10 +6,18 @@ enum Rank: Int {
     case ace = 1
     case two, three, four, five, six, seven, eight, nine, ten
     case jack, queen, king
+    func string() -> String {
+        switch self {
+        case .ace, .jack, .queen, .king:
+            return "\(self)"
+        default:
+            return String(self.rawValue)
+        }
+    }
 }
 
 let c_a = Rank.ace
 let c_2 = Rank.two
-let str_a = "Card a = \(c_a), \(c_a.rawValue)"
-let str_2 = "Card 2 = \(c_2), \(c_2.rawValue)"
+let str_a = "Card a = \(c_a), \(c_a.string())"
+let str_2 = "Card 2 = \(c_2), \(c_2.string())"
 

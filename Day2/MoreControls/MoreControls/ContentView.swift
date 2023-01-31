3a4fed0d791cb1adf071f0c919347a95f466e03b
Kiyong Kim <scgyong@gmail.com>
2023-01-31 14:36:36 +0900

    Toggle switch 에 따라 크기를 다르게 주자.

--- Day2/MoreControls/MoreControls/ContentView.swift
+++ Day2/MoreControls/MoreControls/ContentView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     @State var rotationDegrees = 0.0
+    @State var large = false
     var body: some View {
         VStack {
             Image(systemName: "globe")
@@ -18,12 +19,16 @@ struct ContentView: View {
             Image(systemName: "arrow.right.circle")
                 .resizable()
                 .aspectRatio(1.0, contentMode: .fit)
-                .frame(width: 200, height: 200)
+                .frame(width: large ? 200 : 100, height: 200)
                 .foregroundColor(.yellow)
                 .rotationEffect(.degrees(rotationDegrees * 360))
                 .shadow(color:.red, radius: 10, x: 10, y: 10)
             Slider(value: $rotationDegrees)
-                .frame(width: 200)
+                .padding()
+            Toggle(isOn: $large) {
+                Text("Shows Large Circle")
+            }
+            .padding()
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity)
         .background(

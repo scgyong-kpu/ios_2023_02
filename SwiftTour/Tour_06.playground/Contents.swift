8356211bfeb28f7e0f49570823c8b792713be1dd
Kiyong Kim <scgyong.isu@gmail.com>
2023-01-31 21:32:11 +0900

    A-2. RawValue

--- SwiftTour/Tour_06.playground/Contents.swift
+++ SwiftTour/Tour_06.playground/Contents.swift
@@ -2,13 +2,14 @@ import Foundation
 
 // A. enum
 
-enum Rank {
-    case ace
+enum Rank: Int {
+    case ace = 1
     case two, three, four, five, six, seven, eight, nine, ten
     case jack, queen, king
 }
 
 let c_a = Rank.ace
 let c_2 = Rank.two
-let str_a = "Card a = \(c_a)"
-let str_2 = "Card 2 = \(c_2)"
+let str_a = "Card a = \(c_a), \(c_a.rawValue)"
+let str_2 = "Card 2 = \(c_2), \(c_2.rawValue)"
+

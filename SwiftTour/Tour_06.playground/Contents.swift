636ef5e2c89704d3b24f4d9c2b0a576bcd449c3f
Kiyong Kim <scgyong.isu@gmail.com>
2023-01-31 21:28:17 +0900

    A-1. Introducing enum

--- SwiftTour/Tour_06.playground/Contents.swift
+++ SwiftTour/Tour_06.playground/Contents.swift
@@ -1,3 +1,14 @@
 import Foundation
 
+// A. enum
 
+enum Rank {
+    case ace
+    case two, three, four, five, six, seven, eight, nine, ten
+    case jack, queen, king
+}
+
+let c_a = Rank.ace
+let c_2 = Rank.two
+let str_a = "Card a = \(c_a)"
+let str_2 = "Card 2 = \(c_2)"

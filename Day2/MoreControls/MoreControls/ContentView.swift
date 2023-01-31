102a0547d21cd3489e655a852aa4cb2d7afa273c
Kiyong Kim <scgyong@gmail.com>
2023-01-31 14:27:50 +0900

    Slider 의 값에 의해 회전하도록.

--- Day2/MoreControls/MoreControls/ContentView.swift
+++ Day2/MoreControls/MoreControls/ContentView.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var rotationDegrees = 0.0
     var body: some View {
         VStack {
             Image(systemName: "globe")
@@ -17,8 +18,12 @@ struct ContentView: View {
             Image(systemName: "arrow.right.circle")
                 .resizable()
                 .aspectRatio(1.0, contentMode: .fit)
+                .frame(width: 200, height: 200)
                 .foregroundColor(.yellow)
+                .rotationEffect(.degrees(rotationDegrees * 360))
                 .shadow(color:.red, radius: 10, x: 10, y: 10)
+            Slider(value: $rotationDegrees)
+                .frame(width: 200)
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity)
         .background(

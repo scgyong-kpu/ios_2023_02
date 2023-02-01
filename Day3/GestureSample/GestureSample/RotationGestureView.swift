8d859f7ab4f159540f6de70cccc2d5bc9709bdb9
Kiyong Kim <scgyong@gmail.com>
2023-02-01 14:39:35 +0900

    test in NavigationView

--- Day3/GestureSample/GestureSample/RotationGestureView.swift
+++ Day3/GestureSample/GestureSample/RotationGestureView.swift
@@ -9,12 +9,17 @@ import SwiftUI
 
 struct RotationGestureView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            
+        }
+        .navigationTitle("Rotation Gesture")
     }
 }
 
 struct RotationGestureView_Previews: PreviewProvider {
     static var previews: some View {
-        RotationGestureView()
+        NavigationView {
+            RotationGestureView()
+        }
     }
 }

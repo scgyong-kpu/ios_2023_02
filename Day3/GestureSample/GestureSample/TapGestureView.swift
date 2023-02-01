135ba12396f82b7e085b6fe9948ae4d356371aeb
Kiyong Kim <scgyong@gmail.com>
2023-02-01 14:07:26 +0900

    added LongPress

--- Day3/GestureSample/GestureSample/TapGestureView.swift
+++ Day3/GestureSample/GestureSample/TapGestureView.swift
@@ -10,16 +10,27 @@ import SwiftUI
 struct TapGestureView: View {
     @State var count = 0
     var body: some View {
-        Text("Hello, World!, \(count)")
-            .font(.largeTitle)
-            .foregroundColor(Color.blue)
-            .frame(maxWidth: .infinity)
-            .padding()
-            .background(Color.yellow)
-            .onTapGesture {
-                print("Tap")
-                count += 1
-            }
+        VStack {
+            Text("Tap!, \(count)")
+                .font(.largeTitle)
+                .foregroundColor(Color.blue)
+                .frame(maxWidth: .infinity)
+                .padding()
+                .background(Color.yellow)
+                .onTapGesture {
+                    print("Tap")
+                    count += 1
+                }
+            Text("LongPress!, \(count)")
+                .font(.largeTitle)
+                .foregroundColor(Color.blue)
+                .frame(maxWidth: .infinity)
+                .padding()
+                .background(Color.orange)
+                .onLongPressGesture {
+                    count += 10
+                }
+        }
     }
 }
 

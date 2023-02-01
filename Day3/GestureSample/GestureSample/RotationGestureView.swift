cd718db47a23d2f3eaf8b054f72399571705d99f
Kiyong Kim <scgyong@gmail.com>
2023-02-01 15:01:07 +0900

    rotation 완성

--- Day3/GestureSample/GestureSample/RotationGestureView.swift
+++ Day3/GestureSample/GestureSample/RotationGestureView.swift
@@ -8,8 +8,13 @@
 import SwiftUI
 
 struct RotationGestureView: View {
+    @State var angle = Angle.zero
     var body: some View {
         VStack {
+            Text("Angle = \(String(format: "%.1f", angle.degrees))")
+                .font(.headline)
+                .foregroundColor(.blue)
+            Spacer()
             VStack {
                 Image(systemName: "arrow.right.circle")
                     .font(.largeTitle)
@@ -21,6 +26,17 @@ struct RotationGestureView: View {
             }
             .foregroundColor(.purple)
             .background(Color.orange.opacity(0.3))
+            .rotationEffect(angle)
+            .gesture(
+                RotationGesture()
+                    .onChanged { value in
+                        angle = value
+                    }
+                    .onEnded { _ in
+                        angle = .zero
+                    }
+            )
+            Spacer()
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity)
         .background(Color.blue.opacity(0.2))

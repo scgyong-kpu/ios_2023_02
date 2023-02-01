0f38da51916fb243ef55e4983367116a743fa1ab
Kiyong Kim <scgyong@gmail.com>
2023-02-01 14:39:11 +0900

    Added RotationGestureView

---
+++ Day3/GestureSample/GestureSample/RotationGestureView.swift
@@ -0,0 +1,20 @@
+//
+//  RotationGestureView.swift
+//  GestureSample
+//
+//  Created by Kiyong Kim on 2023/02/01.
+//
+
+import SwiftUI
+
+struct RotationGestureView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RotationGestureView_Previews: PreviewProvider {
+    static var previews: some View {
+        RotationGestureView()
+    }
+}

221f720990e848cc47f4c9399121a73949f46b30
Kiyong Kim <scgyong@gmail.com>
2023-02-01 12:47:46 +0900

    Main -> Navigation -> Tap 형태가 되도록

---
+++ Day3/GestureSample/GestureSample/TapGestureView.swift
@@ -0,0 +1,20 @@
+//
+//  TapGestureView.swift
+//  GestureSample
+//
+//  Created by Kiyong Kim on 2023/02/01.
+//
+
+import SwiftUI
+
+struct TapGestureView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TapGestureView_Previews: PreviewProvider {
+    static var previews: some View {
+        TapGestureView()
+    }
+}

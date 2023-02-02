9ba401329ab9ea8a7a259a4f92f0688049dd60b8
Kiyong Kim <scgyong@gmail.com>
2023-02-02 14:54:26 +0900

    Button 꾸미기

--- Day4/MemoryGame/MemoryGame/GameView.swift
+++ Day4/MemoryGame/MemoryGame/GameView.swift
@@ -11,19 +11,29 @@ struct GameView: View {
     @ObservedObject var gameModel = GameModel()
     var body: some View {
         VStack {
+            Spacer()
             GridStackView(cols: GameModel.cols, rows: GameModel.rows) { row, col in
                 CardView(prefix: "f", card: gameModel.card(row: row, col:col))
                     .onTapGesture {
                         gameModel.toggle(row: row, col: col)
                     }
             }
+            Spacer()
             HStack {
                 Button {
                     gameModel.start()
                 } label: {
                     Text("Restart")
+                        .font(.title)
+                        .padding()
+                        .background(
+                            Capsule()
+                                .stroke(lineWidth: 5)
+                        )
+                        .shadow(color: .gray, radius: 4, x: 3, y: 3)
                 }
             }
+            Spacer()
         }
     }
 }

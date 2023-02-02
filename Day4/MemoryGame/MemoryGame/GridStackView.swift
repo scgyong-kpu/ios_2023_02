1b7ff35e089876d0efd3f425d01f897e865d74d6
Kiyong Kim <scgyong@gmail.com>
2023-02-02 11:40:34 +0900

    범용으로 사용할수 있도록.

--- Day4/MemoryGame/MemoryGame/GridStackView.swift
+++ Day4/MemoryGame/MemoryGame/GridStackView.swift
@@ -7,16 +7,16 @@
 
 import SwiftUI
 
-struct GridStackView: View {
+struct GridStackView<Content: View>: View {
     let cols: Int
     let rows: Int
+    let content: (_ row: Int, _ col: Int)->Content
     var body: some View {
         VStack {
             ForEach(0 ..< rows, id: \.self) { row in
                 HStack {
                     ForEach(0 ..< cols, id: \.self) { col in
-                        Image(systemName: "globe")
-                            .font(.largeTitle)
+                        self.content(row, col)
                     }
                 }
             }
@@ -26,6 +26,11 @@ struct GridStackView: View {
 
 struct GridStackView_Previews: PreviewProvider {
     static var previews: some View {
-        GridStackView(cols: 3, rows: 4)
+        GridStackView(cols: 3, rows: 4) { row, col in
+            Image(systemName: "globe")
+                .resizable()
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+
+        }
     }
 }

6c3c57917d2bbdc3af8d1cc78c4df4a313ab642a
Kiyong Kim <scgyong@gmail.com>
2023-02-02 12:46:45 +0900

    Model 에 있는 card 를 가져온다

--- Day4/MemoryGame/MemoryGame/GameView.swift
+++ Day4/MemoryGame/MemoryGame/GameView.swift
@@ -8,10 +8,11 @@
 import SwiftUI
 
 struct GameView: View {
+    var gameModel = GameModel()
     var body: some View {
         VStack {
             GridStackView(cols: GameModel.cols, rows: GameModel.rows) { row, col in
-                CardView(prefix: "f", card: Card(number: 1))
+                CardView(prefix: "f", card: gameModel.card(row: row, col:col))
             }
         }
     }

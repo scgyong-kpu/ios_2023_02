fcee61fd91aa5b77b37454a8304e594ef3fa4c2d
Kiyong Kim <scgyong@gmail.com>
2023-02-03 11:30:23 +0900

    AlbumStore.load() 완성.

--- Day5/AlbumSample/AlbumSample/AlbumStore.swift
+++ Day5/AlbumSample/AlbumSample/AlbumStore.swift
@@ -21,4 +21,25 @@ class AlbumStore: ObservableObject {
     private static let instance = AlbumStore()
     static func get() -> AlbumStore { instance }
     @Published var albums = [Album]()
+    
+    func load() {
+        guard let url = Bundle.main.url(forResource: "res/albums.json", withExtension: "") else {
+            print("BUG??????")
+            return
+        }
+//        let url = Bundle.main.url(forResource: "res/albums.json", withExtension: "")!
+
+        guard let data = try? Data(contentsOf: url) else {
+            print("Data load failed")
+            return
+        }
+        
+        let decoder = JSONDecoder()
+        guard let albumResponse = try? decoder.decode(AlbumResponse.self, from: data) else {
+            print("Invalid JSON?")
+            return
+        }
+
+        albums = albumResponse.albums
+    }
 }
